e5fe071b7000a0fabdee0b068da1060e5e3678bd
Luana Araujo <luanaa.araujo@hotmail.com>
2016-06-09 20:01:21 -0300

    Primeiro Commit

---
+++ JavaScript1.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+
+    function enviarEmail() {
+        var $requisicao = $.ajax({
+            method: "POST",
+            url: 'http://contato-twsatc.rhcloud.com/contato-api/email',
+            crossDomain: true,
+            data: {
+                nome: $("#nome").val(),
+                email: $("#email").val(),
+                telefone: $("#telefone").val(),
+                mensagem: $("#mensagem").val(),
+                para: $("#para").val()
+            }
+        });
+        $requisicao.then(function (resposta) {
+            $("#carregando").removeClass("show").addClass("hide");
+            if (resposta === true) {
+                $("#contato").trigger("reset");
+                $("#mensagem-sucesso").addClass("show");
+            } else {
+                $("#mensagem-erro").addClass("show");
+            }
+        });
+    }
+
+
+    $("#contato").on("submit", function (event) {
+        event.preventDefault();
+        enviarEmail();
+    });
+
+    $(document).ajaxStart(function () {
+        $("#carregando").addClass("show");
+    });
+});
